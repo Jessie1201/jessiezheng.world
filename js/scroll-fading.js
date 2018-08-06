f984a2b25d4b4365222389c916f6d0d4fff62dc2
Jiaqi Zheng <zhengjiaqi.jessie@gmail.com>
2018-08-06 23:31:08 +0300

    background photo fixed according to right-bottom; add white overlay only for mobile view

--- js/scroll-fading.js
+++ js/scroll-fading.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function($){
   var fadeStart = 100;
   // adjust fadeUntil value according to the number of projects
   var fadeUntil = 1400;
-  var fading = $("#theme-photo");
+  var fading = $(".scroll_fading");
 
   $(window).bind("scroll", function() {
     var offset = $(document).scrollTop();
