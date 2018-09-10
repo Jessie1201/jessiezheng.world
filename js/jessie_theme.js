891c65cfc0dc25af8333515d36c4d2c8bc53420c
Jiaqi Zheng <zhengjiaqi.jessie@gmail.com>
2018-09-11 01:13:41 +0200

    categories emphasis

--- js/jessie_theme.js
+++ js/jessie_theme.js
@@ -1,25 +1,52 @@
 jQuery(function($) {
   // Filter category descriptions
   $(document).ready(function(){
+      $("#all").click(function(){
+          $(this).addClass("category_active");
+          $("#smartcity").removeClass("category_active");
+          $("#ux_interaction").removeClass("category_active");
+          $("#datamining").removeClass("category_active");
+
+          $("#brief_all").fadeIn(800);
+          $("#brief_smartcity").hide();
+          $("#brief_ux").hide();
+          $("#brief_datamining").hide();
+      });
+
       $("#smartcity").click(function(){
+          $(this).addClass("category_active");
+          $("#all").removeClass("category_active");
+          $("#ux_interaction").removeClass("category_active");
+          $("#datamining").removeClass("category_active");
+
+          $("#brief_all").hide();
           $("#brief_smartcity").fadeIn(800);
           $("#brief_ux").hide();
           $("#brief_datamining").hide();
       });
+
       $("#ux_interaction").click(function(){
+          $(this).addClass("category_active");
+          $("#all").removeClass("category_active");
+          $("#smartcity").removeClass("category_active");
+          $("#datamining").removeClass("category_active");
+          
+          $("#brief_all").hide();
           $("#brief_smartcity").hide();
           $("#brief_ux").fadeIn(800);
           $("#brief_datamining").hide();
       });
+
       $("#datamining").click(function(){
+          $(this).addClass("category_active");
+          $("#all").removeClass("category_active");
+          $("#ux_interaction").removeClass("category_active");
+          $("#smartcity").removeClass("category_active");
+
+          $("#brief_all").hide();
           $("#brief_smartcity").hide();
           $("#brief_ux").hide();
           $("#brief_datamining").fadeIn(800);
-      });
-      $("#all").click(function(){
-          $("#brief_smartcity").hide();
-          $("#brief_ux").hide();
-          $("#brief_datamining").hide();
       });
   });
 
